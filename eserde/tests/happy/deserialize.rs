528d3b54e0e360e9f97e021ffbe21990dacc608e
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2025-01-24 16:20:30 +0100

    Add a test to check that serde attributes are preserved and behave as expected

--- eserde/tests/happy/deserialize.rs
+++ eserde/tests/happy/deserialize.rs
@@ -11,3 +11,25 @@ struct TupleStructOneField(#[serde(default)] Option<u32>);
 
 #[derive(eserde::Deserialize)]
 struct TupleStructMultipleFields(#[serde(default)] Option<u32>, u32, u64);
+
+#[test]
+fn deserialize() {
+    let payloads = [
+        r#"{
+            "b": 5,
+            "c": [[1, 2, 3], [4, 5, 6]]
+        }"#,
+        r#"{
+            "a": 5,
+            "b": null,
+            "c": [[null, 2, 3], [4, 5, 6]]
+        }"#,
+    ];
+    for payload in payloads {
+        assert!(
+            serde_json::from_str::<NamedStruct>(payload).is_ok(),
+            "Failed to deserialize: {}",
+            payload
+        );
+    }
+}
