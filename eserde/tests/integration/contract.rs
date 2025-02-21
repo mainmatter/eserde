edad4c37122869e0d69d8a96e60e4dcd225c0dd6
Henk Oordt <hd@oordt.dev>
2025-02-21 11:46:23 +0100

    Inline test inputs and expected values in integration test

--- eserde/tests/integration/contract.rs
+++ eserde/tests/integration/contract.rs
@@ -1,32 +1,110 @@
+use eserde_test_helper::assert_from_json_inline;
 use eserde_test_helper::contract::*;
 use eserde_test_helper::test;
 
 #[test]
 fn struct_deny_unknown_fields() {
-    test!(StructDenyUnknownFields).from_json_assert_snapshot();
+    let test = test!(serialized; StructDenyUnknownFields,
+        r#"{"DEFAULT":true,"SKIP-SERIALIZING-IF":true,"renamed":false,"OPTION":false}"#
+    );
+    assert_from_json_inline!(test, @r#"
+    Err(
+        DeserializationErrors(
+            [
+                DeserializationError {
+                    path: Some(
+                        Path {
+                            segments: [],
+                        },
+                    ),
+                    details: "missing field `write_only`",
+                },
+            ],
+        ),
+    )
+    "#);
 }
 
 #[test]
 fn struct_allow_unknown_fields() {
-    test!(StructAllowUnknownFields).from_json_assert_snapshot();
+    let test = test!(serialized; StructAllowUnknownFields,
+        r#"{"DEFAULT":false,"renamed":false,"OPTION":null}"#
+    );
+    assert_from_json_inline!(test, @r#"
+    Err(
+        DeserializationErrors(
+            [
+                DeserializationError {
+                    path: Some(
+                        Path {
+                            segments: [],
+                        },
+                    ),
+                    details: "missing field `write_only`",
+                },
+                DeserializationError {
+                    path: Some(
+                        Path {
+                            segments: [],
+                        },
+                    ),
+                    details: "missing field `skip_serializing_if`",
+                },
+            ],
+        ),
+    )
+    "#);
 }
 
 #[test]
 fn tuple_struct() {
-    test!(TupleStruct).from_json_assert_snapshot();
+    let test = test!(serialized; TupleStruct,
+        r#"["XVUgTUKTJ7J8r","yZwcp1Ge","nf9hN3"]"#
+    );
+    assert_from_json_inline!(test, @r#"
+    Ok(
+        TupleStruct(
+            "XVUgTUKTJ7J8r",
+            false,
+            "yZwcp1Ge",
+            "nf9hN3",
+        ),
+    )
+    "#);
 }
 
 #[test]
 fn externally_tagged_enum() {
-    test!(ExternalEnum).from_json_assert_snapshot();
+    let test = test!(serialized; ExternalEnum,
+        r#""renamed_unit""#
+    );
+    assert_from_json_inline!(test, @r"
+    Ok(
+        RenamedUnit,
+    )
+    ");
 }
 
 #[test]
 fn internally_tagged_enum() {
-    test!(InternalEnum).from_json_assert_snapshot();
+    let test = test!(serialized; InternalEnum,
+        r#"{"tag":"renamed_unit"}"#
+    );
+    assert_from_json_inline!(test, @r"
+    Ok(
+        RenamedUnit,
+    )
+    ");
 }
 
 #[test]
 fn adjacently_tagged_enum() {
-    test!(AdjacentEnum).from_json_assert_snapshot();
+    let test = test!(serialized; AdjacentEnum,
+        r#"{"tag":"renamed_unit"}"#
+    );
+    assert_from_json_inline!(test, @r"
+    Ok(
+        RenamedUnit,
+    )
+    ");
 }
--- eserde/tests/integration/enum_repr.rs
+++ eserde/tests/integration/enum_repr.rs
@@ -1,7 +1,17 @@
+use eserde_test_helper::assert_from_json_inline;
 use eserde_test_helper::enum_repr::*;
 use eserde_test_helper::test;
 
 #[test]
 fn enum_repr() {
-    test!(Enum).from_json_assert_snapshot();
+    let test = test!(serialized; Enum,
+        r#"5"#
+    );
+    assert_from_json_inline!(test, @r"
+    Ok(
+        Enum(
+            Five,
+        ),
+    )
+    ");
 }
