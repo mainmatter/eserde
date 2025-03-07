7b7f1920008871fe09f3116f9bf0a40b19e7ce6f
Mingwei Samuel <mingwei.samuel@gmail.com>
2025-03-06 17:07:37 -0800

    fix json test

--- eserde/tests/json_types.rs
+++ eserde/tests/json_types.rs
@@ -3,16 +3,16 @@
 
 use std::collections::HashMap;
 
-// TODO: these should give more error messages than the first.
 #[test]
 fn test() {
     use serde_json::value::{Map, Number, Value};
 
     const PAYLOAD: &str = r#"{
         "a": -0.0,
-        "b": { "a2": -5 },
+        "b": true,
         "c": 8,
-        "d": { "a2": false }
+        "d": { "a2": false },
+        "e": "foo"
     }"#;
 
     assert!(serde_json::from_str::<Value>(PAYLOAD).is_ok());
@@ -20,9 +20,12 @@ fn test() {
 
     let result = eserde::json::from_str::<HashMap<String, Number>>(PAYLOAD);
     let errors = result.unwrap_err();
+    // Note the errors degrade after the `"d": {` due to the curly brace.
     insta::assert_snapshot!(errors, @r###"
     Something went wrong during deserialization:
-    - b: invalid type: map, expected a JSON number at line 3 column 15
+    - b: invalid type: boolean `true`, expected a JSON number at line 3 column 17
+    - d: invalid type: map, expected a JSON number at line 5 column 15
+    - expected `,` or `}` at line 5 column 16
     "###
     );
 }
